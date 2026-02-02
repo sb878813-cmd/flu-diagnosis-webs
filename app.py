d45d4c03917a561ec99bc2abd658508b85fc79b0
sb878813-cmd <sb878813@gmail.com>
2026-02-02 07:11:06 +0530

    app.py

--- app.py
+++ app.py
@@ -102,5 +102,10 @@ def download():
     return send_file(filename, as_attachment=True)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+
